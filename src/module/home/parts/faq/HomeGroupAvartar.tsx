9d5150a71abfa179a4182d1d2da8d1e3d1a23d30
ThanhPhongsof <ttphongletter@gmail.com>
2022-08-09 13:52:03 +0700

    fix(home) : update avatar

--- src/module/home/parts/faq/HomeGroupAvartar.tsx
+++ src/module/home/parts/faq/HomeGroupAvartar.tsx
@@ -5,17 +5,17 @@ const HomeGroupAvartar = () => {
   return (
     <div className="flex jusity-center items-center my-6 md:my-8">
       <Avartar
-        src="https://s3-alpha-sig.figma.com/img/74c6/0f24/857ed03c48e8a06aff8d4bced17c0656?Expires=1659916800&Signature=SgQPg91JNTL8UWyxUnNkqRD3vTy2d7RfQcQjAAQAlzCWZJ3HAVkqZz1wJRfKQ~LLCPpzsJ31I8fCmi1VWYx6LC10DaI54tp70PCkCa6Q2T7anLGBhm~UBxpjkyLqx8oPGqljtjI~oquRZk5ikUDtKog3uD7YhhMoR6Do5yLG8IO028Mvm7~kftoaD-BXdY9NRf5izfhvWiDHsmjVswIeM5Y11EqNNkAp9tzfXXHAcl9fkRvshGf7~V8~QhlOqtn2MwK6sU1pclXijztWfhi~wYk97vUiGyRkge9yyTe2mbhBLNLQlQaxYUNCIt2cveaTFDQCVOzACE0hadN7LcyaLQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+        src="https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
         className="bg-[#ABB677] w-12 h-12 translate-x-4"
         alt="Alec Whitten"
       ></Avartar>
       <Avartar
-        src="https://s3-alpha-sig.figma.com/img/2f11/9087/0d753151f58657595136f67c584b5c8c?Expires=1659916800&Signature=OYf4Evcrv5TRWuyEMQd6if1OVsvAOALT1CS1BjvQHoINnzLkEYDuNorXi2J34hmw1eIjCvClTxtaDcG3XN46ve4TSe1T1Eqb7QdOngr6AI0e77MvKbsoA0wKh-docRIjL9r4FrU8L2C5GvzET8pHoqHe2W2i-b3L3VfeoK9XG7hXDwOG7wcNr9~dllCpkiRmIkp48dWSOOrz1pJGgW50DLliXdcfD8KmmFTmDqhyF4NJryIfNYShL0rT2Wpe1S0OsnO5lO6bUkDuLSxWIj2uk97kDGEvXyBFHLi58X1d5w4s~6vm5mgd3Drt5SsQ2AAO4QF8zWDrDkYcesp~QhhUxA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
         className="bg-[#C7B9DA] border-2 border-white w-14 h-14"
         alt="Olivia Rhye"
       ></Avartar>
       <Avartar
-        src="https://s3-alpha-sig.figma.com/img/f8b5/1203/5a2e42c8ec0c92cd29eb940180f4d6b5?Expires=1659916800&Signature=UNVBlW1EuYb7JUYJ0UYhmlr-3PzDUUpqnZmqL8X2t6zlcsI6e68wvEWMwVCV454Adc65Tx1WPheWWBvOsrVKTVaM06krAwKzcrpd6GryjgmRcYqX4RR2kasyPlHtgCeQvjuaR8bcOwBJvl7mI2w0RPfw8aF~0~j2vEGOpwAw51zlL5YH4i2O29JmAJ4QVHG-t5leaOj2tHX0xGaeP9A~yoLKL1peIoFPpJuYdHeh4YJMzhP1azgNXgd1~Qd8eWnC9IKIoJnXQI8zh6VmvIqU2P6PoqYCA-GfBfxB9wyWNmMWFeQRizzerE4Z3EHEMDfJdtSunY3XB2HP0CYxUF8M4Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+        src="https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
         className="bg-[#D9B9BB] w-12 h-12 -translate-x-4"
         alt="Lori Bryson"
       ></Avartar>
